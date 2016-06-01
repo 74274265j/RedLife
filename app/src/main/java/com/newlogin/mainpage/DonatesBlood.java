ed7f887fca4eb527e42003f761420b01fbfe1e2e
Neeraj43 <neruwadhwani@gmail.com>
2016-06-01 20:59:02 +0530

    Donor connected

--- app/src/main/java/com/newlogin/mainpage/DonatesBlood.java
+++ app/src/main/java/com/newlogin/mainpage/DonatesBlood.java
@@ -19,7 +19,7 @@ public class DonatesBlood extends AppCompatActivity implements View.OnClickListe
 
     private EditText editTextName;
     private EditText editTextbloodgrp;
-    private EditText editTextage;
+    private EditText editTextdate;
     private EditText editTextaddress;
     private EditText editTextcontactno;
 
@@ -35,7 +35,7 @@ public class DonatesBlood extends AppCompatActivity implements View.OnClickListe
 
         editTextName = (EditText) findViewById(R.id.et1);
         editTextbloodgrp = (EditText) findViewById(R.id.et2);
-        editTextage = (EditText) findViewById(R.id.et3);
+        editTextdate = (EditText) findViewById(R.id.et3);
         editTextaddress = (EditText) findViewById(R.id.et4);
         editTextcontactno = (EditText) findViewById(R.id.et5);
 
@@ -55,14 +55,14 @@ public class DonatesBlood extends AppCompatActivity implements View.OnClickListe
     private void registerUser() {
         String name = editTextName.getText().toString().trim().toLowerCase();
         String bloodgrp = editTextbloodgrp.getText().toString().trim().toLowerCase();
-        String age = editTextage.getText().toString().trim().toLowerCase();
+        String date = editTextdate.getText().toString().trim().toLowerCase();
         String address = editTextaddress.getText().toString().trim().toLowerCase();
         String contactno = editTextcontactno.getText().toString().trim().toLowerCase();
 
-        register(name, bloodgrp, age, address,contactno);
+        register(name, bloodgrp, date, address,contactno);
     }
 
-    private void register(String name, String bloodgrp, String age, String address, String contactno) {
+    private void register(String name, String bloodgrp, String date, String address, String contactno) {
         class RegisterUser extends AsyncTask<String, Void, String> {
             ProgressDialog loading;
             RegisterUserClass ruc = new RegisterUserClass();
@@ -87,7 +87,7 @@ public class DonatesBlood extends AppCompatActivity implements View.OnClickListe
                 HashMap<String, String> data = new HashMap<String, String>();
                 data.put("name", params[0]);
                 data.put("bloodgrp", params[1]);
-                data.put("age", params[2]);
+                data.put("date", params[2]);
                 data.put("address", params[3]);
                 data.put("contactno", params[4]);
 
@@ -98,6 +98,6 @@ public class DonatesBlood extends AppCompatActivity implements View.OnClickListe
         }
 
         RegisterUser ru = new RegisterUser();
-        ru.execute(name, bloodgrp, age, address, contactno);
+        ru.execute(name, bloodgrp, date, address, contactno);
     }
 }
